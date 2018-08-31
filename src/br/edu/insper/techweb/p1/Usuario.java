f9d269cb4c44c30faca6a29fecc11b883ff806b4
Otofuji <ericfoa@al.insper.edu.br>
2018-08-31 08:42:25 -0300

    Correção de erros

---
+++ src/br/edu/insper/techweb/p1/Usuario.java
@@ -0,0 +1,21 @@
+package br.edu.insper.techweb.p1;
+
+
+
+public class Usuario {
+	private Integer id_usuario;
+	private String email;
+	private String nome;
+	private String sobrenome;
+	
+	public Integer getIdUsuario() {return this.id_usuario;}
+	public void setIdUsuario(Integer id_usuario) {this.id_usuario = id_usuario;}
+	public String getEmail() {return this.email;}
+	public void setEmail(String email) {this.email = email;}
+	public String getNome() {return this.nome;}
+	public void setNome(String nome) {this.nome = nome;}
+	public String getSobrenome() {return this.sobrenome;}
+	public void setSobrenome(String sobrenome) {this.sobrenome = sobrenome;}
+	
+
+}
