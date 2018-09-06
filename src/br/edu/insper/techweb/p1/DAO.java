1f351ecce33de8dd7e8b8d3a85b59f11bb7ef6c7
Otofuji <ericfoa@al.insper.edu.br>
2018-09-06 08:14:10 -0300

    DAO

--- src/br/edu/insper/techweb/p1/DAO.java
+++ src/br/edu/insper/techweb/p1/DAO.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataAccessObject {
+public class DAO {
 	private Connection connection = null;
 	
-	public void dataAccessObject() {
+	public void dao() {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
