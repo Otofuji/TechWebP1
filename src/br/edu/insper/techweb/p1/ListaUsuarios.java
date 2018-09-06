db96b990197095a5a1e947e7561732b71d21e4a7
Otofuji <ericfoa@al.insper.edu.br>
2018-09-06 09:36:38 -0300

    DAO

--- src/br/edu/insper/techweb/p1/ListaUsuarios.java
+++ src/br/edu/insper/techweb/p1/ListaUsuarios.java
@@ -11,16 +11,17 @@ import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 @WebServlet("/lista")
 
-public class Lista {
+public class ListaUsuarios {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		DAO dao = new DAO();
 		
 		List<Usuarios> usuarios = dao.getListaUsuarios();
-		List<Categorias> categorias = dao.getListaCategorias();
-		List<Notas> notas = dao.getListaNotas();
 		
-		
-		//TODO fazer o GetWriter
+		//TODO fazer isto abaixo
+		PrintWriter out = response.getWriter();
+		out.println("<html><body><table border='1'>");
+		out.println("<tr><td>id_usuario</td>")
+		//TODO fazer isto acima
 		
 		dao.close();
 	}
