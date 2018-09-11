7e3a4f6c8a3407095a2d3efd60e27f0e3c11f3ef
VictorLMoura <victorlaperutademoura@hotmail.com>
2018-09-11 09:33:12 -0300

    null statements

--- src/br/edu/insper/techweb/p1/DAO.java
+++ src/br/edu/insper/techweb/p1/DAO.java
@@ -123,43 +123,22 @@ public class DAO {
 	
 	public List<Notas> getListaNotas() {
 		List<Notas> notas = new ArrayList<Notas>();
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement("SELECT * FROM Notas");
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		ResultSet rs = null;
-		try {
-			rs = stmt.executeQuery();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		
-		try {
+			PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Notas");
+			ResultSet rs = stmt.executeQuery();
+			
 			while (rs.next()) {
 				Notas nota = new Notas();
 				nota.setIdNota(rs.getInt("id_nota"));
 				nota.setTipoNota(rs.getInt("tipo_nota"));
 				nota.setConteudoNota(rs.getString("conteudo_nota"));
 				notas.add(nota);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+				rs.close();
+				stmt.close();
 		}
-		
-		try {
-			rs.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			stmt.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
+		}catch (SQLException e1) {
+			e1.printStackTrace();
+		}		
 		return notas;
 		
 	}
@@ -179,9 +158,8 @@ public class DAO {
 	
 	public void adicionaUsuario(Usuarios usuario) {
 		String sql = "INSERT INTO Usuarios" + "(id_usuario,email,nome,sobrenome,senha) values(?,?,?,?,?)";
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
 			stmt.setInt(1, usuario.getIdUsuario());
 			stmt.setString(2, usuario.getEmail());
 			stmt.setString(3, usuario.getNome());
@@ -198,9 +176,8 @@ public class DAO {
 	
 	public void adicionaCategoria(Categorias categoria) {
 		String sql = "INSERT INTO Categorias" + "(id_categoria,categoria) values(?,?)";
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
 			stmt.setInt(1, categoria.getIdCategoria());
 			stmt.setString(2, categoria.getCategoria());
 			stmt.execute();
@@ -213,9 +190,8 @@ public class DAO {
 	
 	public void adicionaNota(Notas nota) {
 		String sql = "INSERT INTO Notas" + "(id_nota,tipo_nota,conteudo_nota) values(?,?,?)";
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
 			stmt.setInt(1, nota.getIdNota());
 			stmt.setInt(2, nota.getTipoNota());
 			stmt.setString(3, nota.getConteudoNota());
@@ -231,9 +207,8 @@ public class DAO {
 	public void alteraUsuario(Usuarios usuario) {
 		
 		String sql = "UPDATE Usuarios SET " + "email=?, nome=?, sobrenome=? WHERE id_usuario=?";
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
 			stmt.setString(1,  usuario.getEmail());
 			stmt.setString(2, usuario.getNome());
 			stmt.setString(3,  usuario.getSobrenome());
@@ -249,9 +224,8 @@ public class DAO {
 	
 	public void alteraCategoria(Categorias categoria) {
 		String sql = "UPDATE Usuarios SET " + "categoria=? WHERE id_categoria=?";
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
 			stmt.setString(1,  categoria.getCategoria());
 			stmt.setInt(2,  categoria.getIdCategoria());
 			stmt.execute();
@@ -263,9 +237,8 @@ public class DAO {
 	
 	public void alteraNota(Notas nota) {
 		String sql = "UPDATE Notas SET " + "tipo_nota=?, conteudo_nota=? WHERE id_nota=?";
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
 			stmt.setInt(1, nota.getTipoNota());
 			stmt.setString(2, nota.getConteudoNota());
 			stmt.setInt(3, nota.getIdNota());
@@ -278,9 +251,8 @@ public class DAO {
 	
 	
 	public void removeUsuario(Integer id_usuario) {
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement("DELETE FROM Usuarios WHERE id=?");
+			PreparedStatement stmt = connection.prepareStatement("DELETE FROM Usuarios WHERE id=?");
 			stmt.setLong(1, id_usuario);
 			stmt.execute();
 			stmt.close();
@@ -290,9 +262,8 @@ public class DAO {
 	}
 	
 	public void removeCategoria(Integer id_categoria) {
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement("DELETE FROM Categorias WHERE id+?");
+			PreparedStatement stmt = connection.prepareStatement("DELETE FROM Categorias WHERE id+?");
 			stmt.setLong(1, id_categoria);
 			stmt.execute();
 			stmt.close();
@@ -302,9 +273,8 @@ public class DAO {
 	}
 	
 	public void removeNota(Integer id_nota) {
-		PreparedStatement stmt = null;
 		try {
-			stmt = connection.prepareStatement("DELETE FROM Notas WHERE id_nota=?");
+			PreparedStatement stmt = connection.prepareStatement("DELETE FROM Notas WHERE id_nota=?");
 			stmt.setLong(1,  id_nota);
 			stmt.execute();
 			stmt.close();
