f7e47a72833d9c26a561cfe251e59fcda13d326f
Otofuji <ericfoa@al.insper.edu.br>
2018-09-14 21:19:06 -0300

    CRUD

---
+++ src/br/edu/insper/techweb/p1/AlteraTexto.java
@@ -0,0 +1,53 @@
+package br.edu.insper.techweb.p1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/alteraTexto")
+public class AlteraTexto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+   
+	public AlteraTexto() {
+		super();
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+            }
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		DAO dao = new DAO();
+		String notaId_string = request.getParameter("notaId");
+		int notaId = Integer.parseInt(notaId_string);
+		dao.alteraNota(notaId);
+		request.getRequestDispatcher("result.jsp").forward(request, response);
+	}
+	
+	
+
+}
